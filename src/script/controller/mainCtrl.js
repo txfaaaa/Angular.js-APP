ee284440dcfe2492bef7c6ccd2d476c85678d673
txfaaaa <1061318171@qq.com>
2018-04-19 13:48:56 +0800

    Update mainCtrl.js

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('app').controller('mainCtrl',['$http','$scope',function($http,$scope){
-  $http({method: 'GET',url: '/data/positionList.json'})
+  $http({method: 'GET',url: 'data/positionList.json'})
   .then(function successCallback(resp) {
     console.log(resp);
     $scope.list= resp.data;
