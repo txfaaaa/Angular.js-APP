d66f82126d09d8b97edaa40ece05b21226445190
txfaaaa <1061318171@qq.com>
2018-04-19 13:47:36 +0800

    Update favoriteCtrl.js

--- src/script/controller/favoriteCtrl.js
+++ src/script/controller/favoriteCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('app').controller('favoriteCtrl',['$http','$scope',function($http,$scope){
-  $http({method: 'GET',url: '/data/myfavorite.json'})
+  $http({method: 'GET',url: 'data/myFavorite.json'})
   .then(function successCallback(resp) {
     console.log(resp);
     $scope.list= resp.data;
