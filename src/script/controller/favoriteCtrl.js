448c1ecd7e5b241f12539008b37b04d2c15fd643
txfaaaa <1061318171@qq.com>
2018-04-19 09:06:51 +0800

    移除src目录下所有文件的版本控制

--- src/script/controller/favoriteCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('app').controller('favoriteCtrl',['$http','$scope',function($http,$scope){
-  $http({method: 'GET',url: '/data/myfavorite.json'})
-  .then(function successCallback(resp) {
-    console.log(resp);
-    $scope.list= resp.data;
-  });
-}])
